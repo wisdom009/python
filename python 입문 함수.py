2d0b03c77c97c7d54f44fd326f10e90cde57e797
wisdom009 <hangul1115@naver.com>
2019-07-15 17:30:27 +0900

    a

---
+++ python 입문 함수.py
@@ -0,0 +1,52 @@
+print('hello')
+print(hello) # '' 문자 등록에는 필수
+print('hello');print('hi') # ;  연속적으로 다음라인에 출력
+a = 10
+if a == 10:
+  print("a is ten") # :는 모함수와 자함루를 나눠준다
+
+if a == 10
+print("a is ten") 
+# 출적은 하지만 모함수와 다르게 출력만함 일정한 법칙이 적용하지 않음
+
+
+# IndentationError: expected an indented block 모와 자를 구별하는 공간이 없어서 나오는 에러
+a = 10
+if a == 10:
+  print("it is")
+  print("ten") # 각기 다른 줄에 출력
+
+
+5/2
+5//2 # 나눈 숫자 표시 
+5.5//2 # 소수점까지 표시될경우 같이 표시
+4.0/2
+
+2*10 #곱하기
+2**10 # 제곱
+
+int(3.7)
+#int는 소수점 제거 함수
+float(3)
+#float은 소수점 표시 함수 
+
+# divmod(a,b)
+divmod(5,2)
+#2, 1   결과는 a// b , a  % 와 같다고 설명
+
+5 % 
+
+quotient, remainder = divmod(5,2)
+quotient
+remainder
+
+0xf  # 정확히는 뭔지 모름 근데 익숙함,,,,
+
+0.2467 * 12
+0.2467 * 12 + 4.159
+int(0.2467 * 12 + 4.159) # int로 소수점 제거 깔끔!
+
+
+x = 'hello'; y = '10'
+print(y,x) # 동시 출력
+
