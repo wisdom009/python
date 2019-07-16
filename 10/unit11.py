ec3ca4e7307412ae7166771a01cf91e99e82e4a2
wisdom009 <hangul1115@naver.com>
2019-07-16 17:31:08 +0900

    unit09-12

---
+++ 10/unit11.py
@@ -0,0 +1,14 @@
+x =input().split()  #1 2 3 4 5 6 7 8 9 10
+x[0:5]
+x =input().split()
+#oven bat pony total leak wreck curl crop space navy loss knee
+x[0:8]
+
+a = input()
+#python
+b = input()
+#python
+
+print(a[1::2] + b[::2])
+
+
